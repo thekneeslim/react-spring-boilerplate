a08e94501d51fae3190c399a065a9729db63ea52
Denise <denise.lim.sh@gmail.com>
2019-01-30 12:00:47 +0800

    React router

---
+++ frontend/src/components/SampleRouting.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default class SampleRouting extends React.Component {
+
+    render() {
+        return (
+            <h1>Routed Page</h1>
+        )
+    }
+}
